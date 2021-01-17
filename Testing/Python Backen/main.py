9d65bb79a47cc94c0b2bdde5af5267e19367fcfd
Eldar Zulic <69102151+EldarZulic03@users.noreply.github.com>
2021-01-17 16:04:53 -0500

    csv import

--- Testing/Python Backen/main.py
+++ Testing/Python Backen/main.py
@@ -1,3 +1,13 @@
 #- Location: Latitude and Longitude
 #- Nitrogen: 550ppi
 # Libraries Matlib, Pandas, Numpy
+import csv
+
+f = open('C:\Users\Eldar\Documents\GitHub\STS-Hacks\Testing\Python Backen\NO2_err.csv')
+
+csv_f = csv.reader(f)
+
+for row in csv_f:
+    print row
+
+f.close()
